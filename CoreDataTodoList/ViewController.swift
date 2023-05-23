1d78f25cd1dee1fd610c26fa55bae0a37b6855d8
Gaea <f40670@gmail.com>
2023-05-23 18:55:03 +0800

    Initial Commit

---
+++ CoreDataTodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTodoList
+//
+//  Created by 莊雅棋 on 2023/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
